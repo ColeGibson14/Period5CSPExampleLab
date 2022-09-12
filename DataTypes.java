7ed7d175d1b13c8d63db1a24521f27ae516f0a15
Cole Gibson <Cole.Gibson@vtsd.com>
2022-09-12 11:42:02 -0400

    Data Type Update

--- DataTypes.java
+++ DataTypes.java
@@ -6,9 +6,18 @@
 
  */
 public class DataTypes {
+    public static void main(String args[]){
+        int nathanielFavInt = 7;
+        double snicker = 7.5;
+        boolean LikeCats = true;
+        int n1 = 6;
+        int n2 = 5;
+        System.out.println("Nathaniel's Favorite Integer is:" + nathanielFavInt + "and the class snicker was" + snicker);
+        System.out.println(n1 + n2 / n1);
 
-    // Variable syntax: dataType varName = value;
-    int nathanielFavInt = 7;
-    double snicker = 7.5;
-    boolean LikeCats = true;
+
+    }
+
+
 }
+
